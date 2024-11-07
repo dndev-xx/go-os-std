eae7f22eedf251b87c9a8871818dd524fabbdf73
dndevvv <dndev@mail.ru>
2024-11-07 23:16:36 +0300

    feat(hw05): fix

--- hw05_parallel_execution/internal/run.go
+++ hw05_parallel_execution/internal/run.go
@@ -2,6 +2,7 @@ package internal
 
 import (
 	"sync"
+	"sync/atomic"
 
 	"github.com/dndev-xx/go-os-std/hw05_parallel_execution/internal/view" //nolint:depguard
 )
@@ -9,8 +10,8 @@ import (
 type Task func() error
 
 func Run(tasks []Task, n, m int) error { //nolint:gocognit
-	var errCnt int
-	var flag bool
+	var errCnt int32
+	var flag atomic.Bool
 	executionChan := make(chan Task, len(tasks))
 	var wg sync.WaitGroup
 	for i := view.ZERO; i < n; i++ {
@@ -18,14 +19,14 @@ func Run(tasks []Task, n, m int) error { //nolint:gocognit
 		go func() {
 			defer wg.Done()
 			for task := range executionChan {
-				if flag {
+				if flag.Load() {
 					return
 				}
 				if err := task(); m > view.ZERO {
 					if err != nil {
-						errCnt++
-						if errCnt >= m {
-							flag = true
+						atomic.AddInt32(&errCnt, view.UNIT)
+						if int(errCnt) >= m {
+							flag.Store(true)
 							return
 						}
 					}
@@ -36,14 +37,14 @@ func Run(tasks []Task, n, m int) error { //nolint:gocognit
 	go func() {
 		defer close(executionChan)
 		for _, task := range tasks {
-			if m > 0 && errCnt >= m {
+			if m > 0 && int(errCnt) >= m {
 				return
 			}
 			executionChan <- task
 		}
 	}()
 	wg.Wait()
-	if m > view.ZERO && errCnt >= m {
+	if m > view.ZERO && int(errCnt) >= m {
 		return view.ErrErrorsLimitExceeded
 	}
 	return nil
