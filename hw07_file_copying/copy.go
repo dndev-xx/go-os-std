cd995559411d7c6fbbd37951613a609e3f7a71df
dndevvv <dndev@mail.ru>
2024-12-01 22:04:28 +0300

    feat(hw07): execution copying file

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,89 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/cheggaaa/pb/v3" //nolint:depguard
 )
 
 var (
 	ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
+	ErrInvalidLimit          = errors.New("invalid limit")
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	if offset < 0 || offset >= limit {
+		return ErrOffsetExceedsFileSize
+	}
+	if limit < 0 {
+		return ErrInvalidLimit
+	}
+	fromInfo, err := os.Stat(fromPath)
+	if err != nil {
+		return getErr(from, errors.Unwrap(err))
+	}
+	if !fromInfo.Mode().IsRegular() {
+		return getErr(from, ErrUnsupportedFile)
+	}
+	fromSize := fromInfo.Size()
+	if offset > fromSize {
+		return ErrOffsetExceedsFileSize
+	}
+	fromFile, err := os.Open(fromPath)
+	if err != nil {
+		return getErr(from, errors.Unwrap(err))
+	}
+	if offset > 0 {
+		_, err = fromFile.Seek(offset, io.SeekStart)
+		if err != nil {
+			_ = fromFile.Close()
+			return getErr(from, err)
+		}
+	}
+	defer fromFile.Close()
+
+	if fileExists(toPath) {
+		err := os.Remove(toPath)
+		if err != nil {
+			return getErr(from, errors.Unwrap(err))
+		}
+	}
+
+	toFile, err := os.Create(toPath)
+	if err != nil {
+		return getErr(from, errors.Unwrap(err))
+	}
+	defer toFile.Close()
+
+	currentLimit := getLimit(offset, limit, fromSize)
+	bar := pb.Full.Start64(currentLimit)
+	barReader := bar.NewProxyReader(fromFile)
+	_, err = io.CopyN(toFile, barReader, currentLimit)
+	bar.Finish()
+	if err != nil && !errors.Is(err, io.EOF) {
+		return err
+	}
 	return nil
+}
+
+func getErr(from string, err error) error {
+	if err == nil {
+		return nil
+	}
+	return fmt.Errorf("from file '%s': %w", from, err)
+}
+
+func fileExists(name string) bool {
+	_, err := os.Stat(name)
+	return !os.IsNotExist(err)
+}
+
+func getLimit(offset, limit, fileSize int64) int64 {
+	if limit == 0 || offset+limit > fileSize {
+		return fileSize - offset
+	}
+	return limit
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,73 @@
 package main
 
-import "testing"
+import (
+	"errors"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require" //nolint:depguard
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	tests := []struct {
+		name      string
+		from      string
+		to        string
+		offset    int64
+		limit     int64
+		expectErr error
+	}{
+		{
+			name:      "Error when copying to unsupported file",
+			from:      "testdata/input2.txt",
+			to:        "/dev/urandom",
+			offset:    0,
+			limit:     0,
+			expectErr: ErrUnsupportedFile,
+		},
+
+		{
+			name:      "Error for non-existing source file",
+			from:      "filename",
+			to:        "out.txt",
+			offset:    0,
+			limit:     0,
+			expectErr: nil,
+		},
+		{
+			name:      "Error for negative offset",
+			from:      "testdata/input.txt",
+			to:        "out.txt",
+			offset:    -10,
+			limit:     0,
+			expectErr: nil,
+		},
+		{
+			name:      "Error for no permission on source file",
+			from:      "fromFile",
+			to:        "out.txt",
+			offset:    0,
+			limit:     0,
+			expectErr: nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.name == "Error for no permission on source file" {
+				toFile, _ := os.OpenFile(tt.from, os.O_CREATE|os.O_WRONLY, 0o000)
+				_ = toFile.Close()
+				defer os.Remove(tt.from)
+			}
+
+			err := Copy(tt.from, tt.to, tt.offset, tt.limit)
+
+			if tt.expectErr != nil {
+				require.Error(t, err)
+				require.False(t, errors.Is(err, tt.expectErr))
+			} else {
+				require.Error(t, err)
+			}
+		})
+	}
 }
